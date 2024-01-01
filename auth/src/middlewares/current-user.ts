6043848d4aeacf95435063ecc9d0ca45da619326
ngirimana <chadrackngirimana@gmail.com>
2024-01-01 14:12:56 +0200

    - current user endpints - signout endpoint - current user and auth middlewares

---
+++ auth/src/middlewares/current-user.ts
@@ -0,0 +1,31 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+interface UserPayload {
+    id: string;
+    email: string;
+}
+
+// Augmenting an existing interface
+// we are adding a new property to the Request interface
+// we are adding a new property to the Request interface
+declare global {
+    namespace Express {
+        interface Request {
+            currentUser?: UserPayload;
+        }
+    }
+}
+
+export const currentUser = (req: Request, res: Response, next: NextFunction) => {
+   
+    if (!req.session?.jwt) {
+        return next();
+    }
+    try {
+        const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!) as UserPayload;
+       
+        req.currentUser = payload;
+    } catch (err) {}
+    next();
+};
